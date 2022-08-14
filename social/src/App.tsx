a72b357ed73a9bd5d6aee325dbe88f5e363e09d1
Iona M <51836963+Metodist@users.noreply.github.com>
2022-08-14 18:55:00 +0300

    create 2 function AccordionTitle and AccordionBody

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -2,46 +2,58 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-   <div>
-     <h1>hello!</h1>
-       <Rating />
-       <Accordion />
+    return (
+        <div>
+            <h1>hello!</h1>
+            <Rating/>
+            <Accordion/>
 
-   </div>
-  );
+        </div>
+    );
 }
 
 export default App;
 
-function Accordion (){
-    return(
+function Accordion() {
+    return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
+
         </div>
+
     )
 }
 
-function Rating(){
-    return(
+function Rating() {
+    return (
         <div>
             <h3>Рейтинг</h3>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
 
-function Star (){
-    return(
+function Star() {
+    return (
         <div>star</div>
+    )
+}
+
+function AccordionTitle() {
+    return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }

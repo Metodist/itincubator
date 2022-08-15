a4afa9c4d8d2f977f777e33c2d69aed7654400b8
Iona M <51836963+Metodist@users.noreply.github.com>
2022-08-15 13:41:45 +0300

    samurayWay 06

--- samuraiWay/src/App.tsx
+++ samuraiWay/src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Заголовок</h1>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;

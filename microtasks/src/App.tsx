f1b507db5b9e39b978655580a68a4c798b83a619
Iona M <51836963+Metodist@users.noreply.github.com>
2022-08-19 15:24:56 +0300

    passed 04 lesson - homework is done

--- microtasks/src/App.tsx
+++ microtasks/src/App.tsx
@@ -1,89 +1,30 @@
-import React, {MouseEvent} from 'react';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
 
-/*import {Header} from "./site/Header";
-import {Body} from "./site/Body";
-import {Footer} from "./site/Footer";
-import {NewComponents} from "./components/NewComponents";
-import {CarsComponents} from "./components/CarsComponents";*/
-import {Button} from "./components/Button";
-
-/*type OnClickType = {
-    name: string
-}*/
-
-/*
-const onClickHandler = (name: string) => {
-    return console.log(name)
-}
-*/
-
-
 function App() {
-    // const subscribe=(event:React.MouseEvent<HTMLButtonElement>)=>{
-    //     return console.log('text')
-    // }
+    let[a, setA]=useState(1)
 
-   /* const foo1 = () =>{
-       return console.log(100200)
-
+    const onClickHandler=()=>{
+        setA(++a)
+        console.log(a)
     }
 
-    const foo2 = (a: number) =>{
-        return console.log(a);
-    }
-*/
-
-    const ButtonFoo1=(s: string, age: number)=>{
-        console.log(s, age)
-    }
-
-    const ButtonFoo2=(s: string)=>{
-        console.log(s)
-
+    const onClickHandler2=()=>{
+        setA(0)
+        console.log(a)
     }
 
     return (
-        <>
-            {/*<Header title={'Хедер'}/>
-            <Body titleForBody={'Тело'}/>
-            <Footer titleFooter={'Подвал'}/>
-            <NewComponents students={students}/>
-            <CarsComponents cars={topCars}/>
-            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler('vasya')}>MyYouTubeChannel -1</button>
-            <button onClick={()=>onClickHandler('ivan')}>MyYouTubeChannel -2</button>*/}
+        <div>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
 
-
-
-            <Button name={'button 1'}  callBack={()=>ButtonFoo1('ivan', 21)}/>
-            <Button name={'button 2'} callBack={()=>ButtonFoo2('Vasya')}/>
-        </>
+        </div>
     );
 }
 
 
 export default App;
-
-/*
-const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-]
-
-const topCars = [
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'}
-]*/
-
 
 

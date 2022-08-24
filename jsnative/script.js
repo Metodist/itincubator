63d2fd0cdf526a6e8a0db02d37a0d575a8970880
Iona M <51836963+Metodist@users.noreply.github.com>
2022-08-24 21:04:17 +0300

    start js native

---
+++ jsnative/script.js
@@ -0,0 +1,36 @@
+const students = [
+    {
+        id: 1,
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        id: 2,
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        id: 3,
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        id: 4,
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+
+console.log(getNames(students))
+
+const getNames=(array)=>{
+
+}
